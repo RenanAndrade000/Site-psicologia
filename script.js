f8954e1ed511ef81ad1e62f38689b212041962d5
Renan <qwerty142999@gmail.com>
2023-05-29 00:20:35 -0300

    adding_redirection

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
- 
+
 window.onload = function() {
     var video = document.getElementById("video");
 video.addEventListener('contextmenu', function(e) {
   e.preventDefault();
 });
   };
+
+  function redirect(url){
+    window.location.href = url;
+  }
+  function scrollToSection(section){
+    var section = document.querySelector(section);
+    if (section) {
+      section.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  }
+  
+  function scrollToStart(section){
+      var section = document.querySelector(section);
+      if(section){
+        section.scrollIntoView({
+          behavior: 'smooth'
+        })
+      }
+    }
